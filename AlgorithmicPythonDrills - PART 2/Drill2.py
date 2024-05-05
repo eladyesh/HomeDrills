962a2971b43c24388a78305ad5f5af66d106bd28
eladyesh <102996033+eladyesh@users.noreply.github.com>
2024-05-05 19:27:09 +0300

    Update Drill2.py

--- AlgorithmicPythonDrills - PART 2/Drill2.py
+++ AlgorithmicPythonDrills - PART 2/Drill2.py
@@ -1,5 +1,5 @@
 def pythagorean_triplet_by_sum(sum):
-    """Prints all Pythagorean triplets that sum up to a given number.
+    """ Prints all Pythagorean triplets that sum up to a given number.
 
     Args:
         sum (int): The desired sum of the Pythagorean triplets.
@@ -7,9 +7,11 @@ def pythagorean_triplet_by_sum(sum):
     Returns:
         None
     """
+    
     # We start by iterating over the numbers from the smallest number (a) till sum.
     # Then, we iterate over the numbers that are bigger then a and not bigger then sum.
     # Check if c (which is sum - a - b) is bigger than b and if a ** 2 + b ** 2 == c ** 2
+    
     for a in range(1, sum):
         for b in range(a, sum):
             c = sum - a - b
@@ -19,5 +21,6 @@ def pythagorean_triplet_by_sum(sum):
 
 
 if __name__ == "__main__":
+    
     pythagorean_triplet_by_sum(12)  # 3 < 4 < 5
     pythagorean_triplet_by_sum(120)  # 20 < 48 < 52, 24 < 45 < 51, 30 < 40 < 50
